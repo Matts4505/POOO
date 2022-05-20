e7645950f1a997aa7ce67d12d0b09d55bc8b8f5c
Mateus Carvalho <95925023+Matts4505@users.noreply.github.com>
2022-05-20 13:20:01 +0000

    adicionei arquivos

---
+++ Lista2/primeiro nomee.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program
+{
+    static void Main() {
+        string a,b;
+        string[] texto = Console.ReadLine().Split(" ");
+                 a = (texto[0]);
+                 b = (texto[1]);
+        Console.WriteLine("Bem vindo ao C#," + a );
+
+
+    }
+}
